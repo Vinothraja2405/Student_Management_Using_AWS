17b2bc879b0f840bf00f33fa782978217937edd2
Vinothraja K <vinothofficial105@gmail.com>
2025-03-04 15:37:26 +0530

    Create student_get.py

---
+++ student_get.py
@@ -0,0 +1,17 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('Students')
+
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+
+    return data
